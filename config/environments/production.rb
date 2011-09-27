96cd06493900874f5a068b4b26a547dd481c2b36
Shadley Wentzel <shad6ster@gmai.com>
2011-09-27 21:09:43 +0200

    Removing old files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Moseller::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => '127.0.0.1:3000' }  
 end
