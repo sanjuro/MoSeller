8eb25f8ba0df82adb16e2fff4cb56a2dad7e706b
Shadley Wentzel <shad6ster@gmai.com>
2011-09-30 00:07:41 +0200

    Removed configs

--- config/initializers/mo_sale.rb
+++
@@ -1,7 +0,0 @@
-require 'mo_sale/active_record'
-require 'mo_sale/current_order'
-
-ActionController::Base.class_eval do
-  include ActiveRecord
-  include CurrentOrder
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Moseller::Application.config.secret_token = '2a6c866d9f016e70b469858ffc87ed77db1068b2d4b618cdcdb45a5c84b40bec66d61e52c81e70f9faf728b148e324d86fb4abe5b56f56a1158b084a3193e3b0'
