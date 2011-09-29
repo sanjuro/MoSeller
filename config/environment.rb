4abb2114bee76b94329225e050dfc22b226565a7
Shadley Wentzel <shad6ster@gmai.com>
2011-09-29 15:24:30 +0200

    Removed configs

--- config/environment.rb
+++
@@ -1,8 +0,0 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-Moseller::Application.initialize!
-
-# Include your application configuration below
-ActiveRecord::Base.pluralize_table_names = false
