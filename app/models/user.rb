2463c17b82c3ce71c3141f1a2dc5f0952748b5d7
Shadley Wentzel <shad6ster@gmai.com>
2011-09-30 22:52:26 +0200

    Updated active admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
   attr_accessible :id, :name, :email, :password, :password_confirmation, :remember_me
   
   validates :name,  :presence => true
