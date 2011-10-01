27ed4da0d4cd7f84e79eb845c34aeb11f8d5c174
Shadley Wentzel <shad6ster@gmai.com>
2011-10-01 18:51:17 +0200

    Removed unused classes

---
+++ db/migrate/20110930204814_add_devise_to_user.rb
@@ -0,0 +1,31 @@
+class AddDeviseToUser < ActiveRecord::Migration
+  def self.up
+    change_table(:user) do |t|
+      t.database_authenticatable :null => false
+      t.recoverable
+      t.rememberable
+      t.trackable
+    
+      # t.encryptable
+      # t.confirmable
+      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
+      # t.token_authenticatable
+
+
+      # Uncomment below if timestamps were not included in your original model.
+      # t.timestamps
+    end
+
+    add_index :user, :email,                :unique => true
+    add_index :user, :reset_password_token, :unique => true
+    # add_index :user, :confirmation_token,   :unique => true
+    # add_index :user, :unlock_token,         :unique => true
+    # add_index :user, :authentication_token, :unique => true
+  end
+
+  def self.down
+    # By default, we don't want to make any assumption about how to roll back a migration when your
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::IrreversibleMigration   
+  end
+end
