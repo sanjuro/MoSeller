03a4310598cfa2dddc9496ae84e8a3aadc7be646
Shadley Wentzel <shad6ster@gmai.com>
2011-10-02 19:19:01 +0200

    Updated prices, fixed invoice mailer

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -4,7 +4,6 @@ class InvoiceMailer < ActionMailer::Base
     @invoice = invoice
     @order = order
     @url  = "http://localhost:3000/invoices/" + invoice.id.to_s()
-    attachments["logo.png"] = File.read("#{Rails.root}/public/images/logo.png")  
     mail(:to => invoice.email, :subject => "Your invoice for Order Number "  + order.number)
   end
   
