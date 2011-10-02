41500dffefdcf0995df842744b2bcc7479311172
Shadley Wentzel <shad6ster@gmai.com>
2011-10-02 13:47:54 +0200

    Fixed mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,7 +2,7 @@ class OrderMailer < ActionMailer::Base
 
   def order_email(order)
     @order = order
-    @url  = "http://localhost:3000/orders/" + order.id.to_s()
+    @url  = "http://http://107.20.141.187:3000/orders/" + order.id.to_s()
     mail(:to => order.email, :subject => "Your order has been processed")
   end
   
