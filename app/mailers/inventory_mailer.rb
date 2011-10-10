c19d6b2b7625b19bb55b72cc21839f6c4370e333
Shadley Wentzel <shad6ster@gmai.com>
2011-10-10 22:01:02 +0200

    Update Remail controller

--- app/mailers/inventory_mailer.rb
+++ app/mailers/inventory_mailer.rb
@@ -3,7 +3,7 @@ class InventoryMailer < ActionMailer::Base
   def low_inventory_email(product_source)
     @product_source = product_source
     mail( :to => 'shad6ster@gmail.com', 
-          :from => "headhancho@moseller.com",
+          :from => AppConfig.instance.admin_email,
           :subject => "Stock is running low for " + product_source.description)
   end
   
