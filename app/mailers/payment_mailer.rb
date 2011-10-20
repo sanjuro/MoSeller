4c8fd2120df286a23ef3fe0d3403b86baeeb0a4c
Shadley Wentzel <shad6ster@gmai.com>
2011-10-20 15:03:04 +0200

    Updated Payment mail template

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -4,7 +4,7 @@ class PaymentMailer < ActionMailer::Base
     @email_content = email_content
     mail( :to => "shad6ster@gmail.com", 
           :from => AppConfig.instance.admin_email,
-          :subject => email_content)
+          :subject => "New Payment received")
   end
   
 end
