60feb9cc8ac506a4c3ad756e4c08c447cae7c49d
Shadley Wentzel <shad6ster@gmai.com>
2011-12-15 18:33:49 +0200

    Updated admin

--- app/admin/inventory_level.rb
+++ app/admin/inventory_level.rb
@@ -18,9 +18,7 @@ ActiveAdmin.register InventoryLevel do
     column :clazz
     column :stock_level
     column ("is_current") {|inventory_level| status_tag(inventory_level.state, inventory_level.is_current ? :ok : :error) }
-    column :created_at, :sortable => :created_at do |inventory_level|
-      inventory_level.updated_at
-    end
+    column :created_at
     default_actions
   end
   
