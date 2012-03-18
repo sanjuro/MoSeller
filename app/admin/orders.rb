c4e70201dd96b7988c05243535f550666023967e
sanjuro <shad6ster@gmail.com>
2012-03-18 20:06:49 +0200

    Updated reports

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -126,7 +126,8 @@ ActiveAdmin.register Order do
                "2011-12-01 00:00:00", 
                "2012-01-01 00:00:00", 
                "2012-02-01 00:00:00", 
-               "2012-03-01 00:00:00"]
+               "2012-03-01 00:00:00",
+               "2012-04-01 00:00:00"]
      @orders_by_month = (months).map { |month| Order.count_per_month(Date.parse(month)).to_f}.inspect
 
      @orders_by_day = (10.weeks.ago.to_date..Date.today).map { |date| Order.count_per_day(date).to_f}.inspect 
