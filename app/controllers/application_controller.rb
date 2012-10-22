1d9f43961da13831c0059bef9d246d5b67e3cf2b
sanjuro <shad6ster@gmail.com>
2012-10-22 10:58:00 +0200

    Removed testing for mobile view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,7 @@ class ApplicationController < ActionController::Base
   
   def prepare_for_mobile
     session[:mobile_param] = params[:mobile] if params[:mobile]
-    # request.format = :mobile if mobile_device? == 'mobile'
-    request.format = :mobile
+    request.format = :mobile if mobile_device? == 'mobile'
   end    
     
   def mobile_device?
